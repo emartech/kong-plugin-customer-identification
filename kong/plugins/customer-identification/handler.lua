c03426f9c8f60d1dbb044e687c860d9f8efaf193
bencso <gabor.bencsik13@gmail.com>
2018-08-02 16:12:34 +0200

    INT-0052: set x-suite-customerid header

--- kong/plugins/customer-identification/handler.lua
+++ kong/plugins/customer-identification/handler.lua
@@ -8,10 +8,28 @@ function CustomerIdentificationHandler:new()
   CustomerIdentificationHandler.super.new(self, "customer-identification")
 end
 
+local function set_header(customer_id)
+  ngx.req.set_header('X-Suite-CustomerId', customer_id)
+  return nil
+end
+
 function CustomerIdentificationHandler:access(conf)
   CustomerIdentificationHandler.super.access(self)
 
-  return nil
+  if ngx.req.get_headers()['X-Suite-CustomerId'] then
+    return nil
+  end
+
+  local customer_id = string.match(ngx.var.request_uri, '/api/v2/internal/(.-)/')
+  if customer_id then
+    set_header(customer_id)
+  end
+
+  local customer_id = string.match(ngx.var.request_uri, '/api/services/customers/(.-)/')
+  if customer_id then
+    set_header(customer_id)
+  end
+
 end
 
 return CustomerIdentificationHandler
--- spec/customer-identication/plugin_e2e_spec.lua
+++ spec/customer-identication/plugin_e2e_spec.lua
@@ -11,10 +11,12 @@ local function setup_test_env()
   helpers.dao:truncate_tables()
 
   local service = get_response_body(TestHelper.setup_service())
-  local route = get_response_body(TestHelper.setup_route_for_service(service.id))
+  local route_internal = get_response_body(TestHelper.setup_route_for_service(service.id, '/api/v2/internal'))
+  local route_services = get_response_body(TestHelper.setup_route_for_service(service.id, '/api/services/customers'))
+  local route_custom = get_response_body(TestHelper.setup_route_for_service(service.id, '/custom-service'))
   local plugin = get_response_body(TestHelper.setup_plugin_for_service(service.id, 'customer-identification'))
   local consumer = get_response_body(TestHelper.setup_consumer('TestUser'))
-  return service, route, plugin, consumer
+  return service, route_internal, route_services, route_custom, plugin, consumer
 end
 
 describe("Plugin: customer-identification (access) #e2e", function()
@@ -27,34 +29,63 @@ describe("Plugin: customer-identification (access) #e2e", function()
     helpers.stop_kong(nil)
   end)
 
-  describe("Admin API", function()
-    local service, route, plugin, consumer
+  describe("Customer Identification", function()
+    local service, route_internal, route_services, route_custom, plugin, consumer
 
     before_each(function()
-      service, route, plugin, consumer = setup_test_env()
+      service, route_internal, route_services, route_custom, plugin, consumer = setup_test_env()
     end)
 
-    it("registered the plugin globally", function()
-      local res = assert(helpers.admin_client():send {
-        method = "GET",
-        path = "/plugins/" .. plugin.id,
-      })
-      local body = assert.res_status(200, res)
-      local json = cjson.decode(body)
+    context("when X-Suite-CustomerId header is not present", function()
+      it("should be set if customer id can be found in v2 internal request path", function()
+        local res = assert(helpers.proxy_client():send {
+          method = "GET",
+          path = "/api/v2/internal/12345678/",
+          headers = {
+            ["Host"] = "test1.com"
+          }
+        })
 
-      assert.is_table(json)
-      assert.is_not.falsy(json.enabled)
+        local response = assert.res_status(200, res)
+        local body = cjson.decode(response)
+
+        assert.is_equal('12345678', body.headers["x-suite-customerid"])
+      end)
+
+      it("should be set if customer id can be found in services request path", function()
+        local res = assert(helpers.proxy_client():send {
+          method = "GET",
+          path = "/api/services/customers/12345678/",
+          headers = {
+            ["Host"] = "test1.com"
+          }
+        })
+
+        local response = assert.res_status(200, res)
+        local body = cjson.decode(response)
+
+        assert.is_equal('12345678', body.headers["x-suite-customerid"])
+      end)
     end)
 
-    it("registered the plugin for the api", function()
-      local res = assert(helpers.admin_client():send {
-        method = "GET",
-        path = "/plugins/" ..plugin.id,
-      })
-      local body = assert.res_status(200, res)
-      local json = cjson.decode(body)
-      assert.is_equal(api_id, json.api_id)
+    context("when X-Suite-CustomerId header is present", function()
+      it("should not be set if customer id can be found in the headers", function()
+        local res = assert(helpers.proxy_client():send {
+          method = "GET",
+          path = "/custom-service/",
+          headers = {
+            ["Host"] = "test1.com",
+            ["X-Suite-CustomerId"] = "23456789"
+          }
+        })
+
+        local response = assert.res_status(200, res)
+        local body = cjson.decode(response)
+
+        assert.is_equal('23456789', body.headers["x-suite-customerid"])
+      end)
     end)
+
   end)
 
 end)
