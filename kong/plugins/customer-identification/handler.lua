5fe90a264347e56c67294d788d9060bd68eb74bd
bencso <gabor.bencsik13@gmail.com>
2018-08-03 09:26:04 +0200

    INT-0052: refact pattern matching

--- kong/plugins/customer-identification/handler.lua
+++ kong/plugins/customer-identification/handler.lua
@@ -5,27 +5,33 @@ local CustomerIdentificationHandler = BasePlugin:extend()
 CustomerIdentificationHandler.PRIORITY = 903
 
 function CustomerIdentificationHandler:new()
-  CustomerIdentificationHandler.super.new(self, "customer-identification")
+    CustomerIdentificationHandler.super.new(self, "customer-identification")
+end
+
+local function match_customer_id_from_uri(pattern)
+    local customer_id = string.match(ngx.var.request_uri, pattern)
+    if customer_id then
+        ngx.req.set_header('X-Suite-CustomerId', customer_id)
+        return true
+    end
+
+    return false
 end
 
 function CustomerIdentificationHandler:access(conf)
-  CustomerIdentificationHandler.super.access(self)
+    CustomerIdentificationHandler.super.access(self)
 
-  if ngx.req.get_headers()['X-Suite-CustomerId'] then
-    return nil
-  end
+    if ngx.req.get_headers()['X-Suite-CustomerId'] then
+        return nil
+    end
 
-  local customer_id = string.match(ngx.var.request_uri, '/api/v2/internal/(.-)/')
-  if customer_id then
-    ngx.req.set_header('X-Suite-CustomerId', customer_id)
-    return nil
-  end
+    if match_customer_id_from_uri('/api/v2/internal/(.-)/') then
+        return nil
+    end
 
-  local customer_id = string.match(ngx.var.request_uri, '/api/services/customers/(.-)/')
-  if customer_id then
-    ngx.req.set_header('X-Suite-CustomerId', customer_id)
-    return nil
-  end
+    if match_customer_id_from_uri('/api/services/customers/(.-)/') then
+        return nil
+    end
 
 end
 
